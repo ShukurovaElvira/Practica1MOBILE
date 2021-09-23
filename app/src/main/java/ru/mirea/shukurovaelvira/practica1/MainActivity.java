86cd6d60ddafdda291ce54bc4bdc26c22b7c431d
ShukurovaElvira <72341445+ShukurovaElvira@users.noreply.github.com>
2021-09-23 14:59:02 +0300

    Initial commit

---
+++ app/src/main/java/ru/mirea/shukurovaelvira/practica1/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.mirea.shukurovaelvira.practica1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__second);
+    }
+}
---
+++ clickbutton/src/main/java/ru/mirea/shukurovaelvira/clickbutton/ClickButton.java
@@ -0,0 +1,14 @@
+package ru.mirea.shukurovaelvira.clickbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClickButton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_click_button);
+    }
+}
